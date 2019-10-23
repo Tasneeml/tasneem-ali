d917311a1fba8167b8aaff11dda5cfb8619459f8
tasneem <tasneem.raneem555@gmail.com>
2019-10-23 15:23:40 +0300

    add

---
+++ fun-fnction/js/js.js
@@ -0,0 +1,7 @@
+console.log('hello');
+
+function makecoffe(numberofcubs,style,){//1 argument with type number
+    console.log('makeing coffe');
+}
+makecoffe('not actually a number');
+
---
+++ tasneem.js
@@ -0,0 +1,7 @@
+// 'use strict ';
+// var pugbombButton = document.getElementById('pugbomb');
+// pugbombButton.addEventListener('click', pugbombButtonHandler);
+
+// function pugbombButtonHandler() {
+//   alert('PUGBOMB!!!!');
+// }
